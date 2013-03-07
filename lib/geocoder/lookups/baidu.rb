fd53131834802bc1b3d77797e9844f0557529dc2
Caryl <xianwei.wang@gmail.com>
2013-03-07 15:24:55 +0800

    fix bug

--- lib/geocoder/lookups/baidu.rb
+++ lib/geocoder/lookups/baidu.rb
@@ -4,14 +4,26 @@ require "geocoder/results/baidu"
 module Geocoder::Lookup
   class Baidu < Base
 
+    def name
+      "Baidu"
+    end
+
     def map_link_url(coordinates)
       "http://map.baidu.com?q=#{coordinates.join(',')}"
     end
 
+    def required_api_key_parts
+      ["key"]
+    end
+
+    def query_url(query)
+      "#{protocol}://api.map.baidu.com/geocoder?" + url_query_string(query)
+    end
+
     private # ---------------------------------------------------------------
 
-    def results(query, reverse = false)
-      return [] unless doc = fetch_data(query, reverse)
+    def results(query)
+      return [] unless doc = fetch_data(query)
       case doc['status']; when "OK"
         return doc['result'].blank? ? [] : [doc['result']]
       when "INVALID_PARAMETERS"
@@ -22,14 +34,13 @@ module Geocoder::Lookup
       return []
     end
 
-    def query_url(query, reverse = false)
-      params = {
-        (reverse ? :location : :address) => query,
-        :language => Geocoder::Configuration.language,
+    def query_url_params(query)
+      {
+        (query.reverse_geocode? ? :location : :address) => query,
+        :language => configuration.language,
         :output => 'json',
-        :key => Geocoder::Configuration.api_key
-      }
-      "#{protocol}://api.map.baidu.com/geocoder?" + hash_to_query(params)
+        :key => configuration.api_key
+      }.merge(super)
     end
   end
 end
